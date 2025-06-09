dffd2816311fe7c7272b89b52ccd71044d1b5602
vernellm <vtmangum@gmail.com>
2025-06-09 13:45:48 -0500

    added ground shipping calculation

---
+++ shipping.py
@@ -0,0 +1,21 @@
+# Author: Vernell Mangum
+#
+# Description: Basic python program that calculates your cost of shipping
+# based on shipping prices and weight of parcel
+
+weight = 8.4
+
+# Ground Shipping
+
+ground_ship_cost = 0
+
+if weight <= 2:
+    ground_ship_cost += (weight * 1.50) + 20
+elif weight <= 6:
+    ground_ship_cost += (weight * 3.00) + 20
+elif weight <= 10:
+    ground_ship_cost += (weight * 4.00) + 20
+else:
+    ground_ship_cost += (weight * 4.75) + 20
+    
+print(ground_ship_cost)
